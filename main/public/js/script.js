515385301300f5b9ced96f631a4e870f37980fc7
Fonchio03 <alex.coccia02@gmail.com>
2024-05-10 10:31:03 +0200

    Unione con fra mancante

--- main/public/js/script.js
+++ main/public/js/script.js
@@ -341,13 +341,13 @@ screen.orientation.addEventListener("change", (event) => {
 
 
 $('.circle_est').click(function() {
-    let spWidth = $('.sidepanel_est').width();
-    let spMarginRight = parseInt($('.sidepanel_est').css('margin-right'),10);
+    let spWidth = $('.right-side-panel').width();
+    let spMarginRight = parseInt($('.right-side-panel').css('margin-right'),10);
     let radius = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--radius').trim());
 
     let w = (spMarginRight >= 0 ) ? spWidth * -1 : 0;
     let cw = (w < 0) ? -(w+radius): spWidth-$('.circle_est').width()-radius;
-    $('.sidepanel_est').animate({
+    $('.right-side-panel').animate({
         marginRight:w
     });
     $('.circle_est').animate({
